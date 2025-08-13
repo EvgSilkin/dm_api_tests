700c296178e7f6c1c4b20576c94199b6fc99c3fb
Евгений Силкин <evgsilkin9997@gmail.com>
2025-08-13 17:23:27 +0300

    update test data

--- tests/functional/test_post_v1_account/test_post_v1_account.py
+++ tests/functional/test_post_v1_account/test_post_v1_account.py
@@ -22,7 +22,7 @@ def test_post_v1_account():
 
     account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
-    login = 'create_evg_user_45'
+    login = 'create_evg_user_46'
     password = '123456789'
     email = f'{login}@mail.com'
 
