17d2b8177381cdb680a27bc85c65e2d797c9c2c8
Евгений Силкин <evgsilkin9997@gmail.com>
2025-08-24 00:24:29 +0300

    new push for create context_manager

--- checkers/http_checkers.py
+++ checkers/http_checkers.py
@@ -10,16 +10,13 @@ def check_status_code_http(
         expected_message: str = None
 ):
     try:
-
         yield
-
         if expected_status_code != requests.codes.OK:
             print(expected_status_code, requests.codes.OK)
             raise AssertionError(f"Ожидаемый статус код должен быть равен {expected_status_code}")
         if expected_message:
             raise AssertionError(f"Ожидаемое сообщение: {expected_message}. "
                                  f"Запросы выполнен успешно {expected_status_code}")
-
     except HTTPError as e:
 
         assert e.response.status_code == expected_status_code
