3aa6caaa8dfe834e60606e9bf045991651e3288a
Евгений Силкин <evgsilkin9997@gmail.com>
2025-08-30 00:22:30 +0300

    fix coverage

--- packages/notifier/bot.py
+++ packages/notifier/bot.py
@@ -14,7 +14,7 @@ v.read_in_config()
 def send_file() -> None:
     telegram_bot = TeleBot(v.get("telegram.token"))
     # file_path = Path(__file__).parent.joinpath("../../").joinpath("swagger-doc-dm-api-account.json")
-    file_path = Path(__file__).parent.parent.parent / "swagger-coverage-dm-api-account.json"
+    file_path = Path(__file__).parent.joinpath("../../").joinpath("swagger-coverage-dm-api-account.json")
 
     with open(file_path, 'rb') as document:
         telegram_bot.send_document(
@@ -23,7 +23,7 @@ def send_file() -> None:
             caption='coverage'
         )
 
-    file_path = Path(__file__).parent.parent.parent / "swagger-coverage-dm-api-account.html"
+    file_path = Path(__file__).parent.joinpath("../../").joinpath("swagger-coverage-dm-api-account.html")
 
     with open(file_path, 'rb') as document:
         telegram_bot.send_document(
